fef56c8f7c74eb840193464c6544edfdeb98fe3f
Sam Naser <samnaser@google.com>
2018-12-11 15:08:46 -0800

    Log severity fixes, fix spelling

--- webhook.go
+++ webhook.go
@@ -107,7 +107,7 @@ func mutatePod(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 
 	// determine whether to perform mutation
 	if !mutationRequired(&pod.ObjectMeta) {
-		glog.Errorf("Skipping mutation for %s/%s due to policy check", pod.Namespace, pod.Name)
+		glog.V(3).Infof("Skipping mutation for %s/%s due to policy check", pod.Namespace, pod.Name)
 		return &v1beta1.AdmissionResponse{
 			Allowed: true,
 		}
@@ -183,7 +183,7 @@ func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 	}
-	glog.Errorf("Ready to write reponse ...")
+	glog.V(3).Info("Ready to write response ...")
 	if _, err := w.Write(resp); err != nil {
 		glog.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
